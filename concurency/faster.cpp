4a5a110ad97765216bcace85ff4074d4b3014c7d
Thomas Applencourt <tapplencourt@anl.gov>
2022-03-01 00:45:14 +0000

    Update faster

--- concurency/faster.cpp
+++ concurency/faster.cpp
@@ -1,23 +1,22 @@
-#define MAD_4(x, y)                                                                                                                                                                                    \
-  x = y * x + y;                                                                                                                                                                                       \
-  y = x * y + x;                                                                                                                                                                                       \
-  x = y * x + y;                                                                                                                                                                                       \
+#define MAD_4(x, y)                                                            \
+  x = y * x + y;                                                               \
+  y = x * y + x;                                                               \
+  x = y * x + y;                                                               \
   y = x * y + x;
-#define MAD_16(x, y)                                                                                                                                                                                   \
-  MAD_4(x, y);                                                                                                                                                                                         \
-  MAD_4(x, y);                                                                                                                                                                                         \
-  MAD_4(x, y);                                                                                                                                                                                         \
+#define MAD_16(x, y)                                                           \
+  MAD_4(x, y);                                                                 \
+  MAD_4(x, y);                                                                 \
+  MAD_4(x, y);                                                                 \
   MAD_4(x, y);
-#define MAD_64(x, y)                                                                                                                                                                                   \
-  MAD_16(x, y);                                                                                                                                                                                        \
-  MAD_16(x, y);                                                                                                                                                                                        \
-  MAD_16(x, y);                                                                                                                                                                                        \
+#define MAD_64(x, y)                                                           \
+  MAD_16(x, y);                                                                \
+  MAD_16(x, y);                                                                \
+  MAD_16(x, y);                                                                \
   MAD_16(x, y);
 
 #include <chrono>
 #include <iostream>
 #include <sycl/sycl.hpp>
-
 #define NUM_REPETION 8
 
 template <class T> T busy_wait(long N, T i) {
@@ -30,21 +29,20 @@ template <class T> T busy_wait(long N, T i) {
 }
 
 template <class T>
-void bench(std::vector<std::string> modes, long C_tripcount, bool enable_profiling, bool in_order, int n_queues, bool serial, 
-                        long *total_cpu_time, long* max_cpu_time_command, int *max_index_cpu_time_command) {
+void bench(std::vector<std::string> commands, long kernel_tripcount,
+           bool enable_profiling, bool in_order, int n_queues, bool serial,
+           long *total_cpu_time, long *max_cpu_time_command,
+           int *max_index_cpu_time_command) {
 
   const sycl::device D{sycl::gpu_selector()};
   const int globalWIs = D.get_info<sycl::info::device::sub_group_sizes>()[0];
   const int N = D.get_info<sycl::info::device::max_mem_alloc_size>() / sizeof(T);
 
   const sycl::context C(D);
-
-  if (n_queues == -1)
-    n_queues = modes.size();
-
   sycl::property_list pl;
   if (enable_profiling && in_order)
-    pl = sycl::property_list{sycl::property::queue::in_order{}, sycl::property::queue::enable_profiling{}};
+    pl = sycl::property_list{sycl::property::queue::in_order{},
+                             sycl::property::queue::enable_profiling{}};
   else if (enable_profiling)
     pl = sycl::property_list{sycl::property::queue::enable_profiling{}};
   else if (in_order)
@@ -55,9 +53,9 @@ void bench(std::vector<std::string> modes, long C_tripcount, bool enable_profili
     Qs.push_back(sycl::queue(C, D, pl));
 
   std::vector<std::vector<T *>> buffers;
-  for (auto &mode : modes) {
+  for (auto &command : commands) {
     std::vector<T *> buffer;
-    for (const char &t : mode) {
+    for (const char &t : command) {
       T *b;
       if (t == 'M') {
         b = static_cast<T *>(malloc(N * sizeof(T)));
@@ -72,17 +70,19 @@ void bench(std::vector<std::string> modes, long C_tripcount, bool enable_profili
     buffers.push_back(buffer);
   }
 
-  *total_cpu_time = std::numeric_limits<long>::max() ;
+  *total_cpu_time = std::numeric_limits<long>::max();
   for (int r = 0; r < NUM_REPETION; r++) {
     std::vector<long> cpu_times;
     auto s0 = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < modes.size(); i++) {
+    for (int i = 0; i < commands.size(); i++) {
       const auto s = std::chrono::high_resolution_clock::now();
       sycl::queue Q = Qs[i % n_queues];
       sycl::event event;
-      if (modes[i] == "C") {
+      if (commands[i] == "C") {
         T *ptr = buffers[i][0];
-        Q.parallel_for(globalWIs, [ptr, C_tripcount](sycl::item<1> j) { ptr[j] = busy_wait(C_tripcount, (T)j); });
+        Q.parallel_for(globalWIs, [ptr, kernel_tripcount](sycl::item<1> j) {
+          ptr[j] = busy_wait(kernel_tripcount, (T)j);
+        });
       } else {
         Q.copy(buffers[i][1], buffers[i][0], N);
       }
@@ -90,48 +90,133 @@ void bench(std::vector<std::string> modes, long C_tripcount, bool enable_profili
       if (serial) {
         Q.wait();
         const auto e = std::chrono::high_resolution_clock::now();
-        cpu_times.push_back(std::chrono::duration_cast<std::chrono::microseconds>(e - s).count());
+        cpu_times.push_back(
+            std::chrono::duration_cast<std::chrono::microseconds>(e - s)
+                .count());
       }
     }
     for (auto &Q : Qs)
       Q.wait();
     const auto e0 = std::chrono::high_resolution_clock::now();
-    const auto curent_total_cpu_time = std::chrono::duration_cast<std::chrono::microseconds>(e0 - s0).count();
-  if (curent_total_cpu_time < *total_cpu_time) {
-    *total_cpu_time = curent_total_cpu_time; 
-    if (serial) {
-        *max_index_cpu_time_command = std::distance(cpu_times.begin(), std::max_element(cpu_times.begin(), cpu_times.end()));
+    const auto curent_total_cpu_time =
+        std::chrono::duration_cast<std::chrono::microseconds>(e0 - s0).count();
+    if (curent_total_cpu_time < *total_cpu_time) {
+      *total_cpu_time = curent_total_cpu_time;
+      if (serial) {
+        *max_index_cpu_time_command =
+            std::distance(cpu_times.begin(),
+                          std::max_element(cpu_times.begin(), cpu_times.end()));
         *max_cpu_time_command = cpu_times[*max_index_cpu_time_command];
+      }
     }
-  }
   }
   for (const auto &buffer : buffers)
     for (const auto &ptr : buffer)
-      (sycl::get_pointer_type(ptr, C) != sycl::usm::alloc::unknown) ? sycl::free(ptr, C) : free(ptr);
+      (sycl::get_pointer_type(ptr, C) != sycl::usm::alloc::unknown)
+          ? sycl::free(ptr, C)
+          : free(ptr);
+}
+
+void print_help_and_exit(std::string msg) {
+  std::cout << "ERROR: " << msg << std::endl;
+  const char *help = "Usage: ./a.out  (--in_order | --out_of_order)\n"
+                     "                [--enable_profiling]\n"
+                     "                [--n_queues=<queues>]\n"
+                     "                [--kernel_tripcount=<tripcount>]\n"
+                     "                COMMAND...\n"
+                     "\n"
+                     "Options:\n"
+                     "--kernel_tripcount       [default: 10000]\n"
+                     "--n_queues=<nqueues>     [default: -1]. One queue when "
+                     "out_of_order , one per COMMANDS when in order\n"
+                     "COMMAND                  [possible values: C,MD,DM]\n";
+  std::cout << help << std::endl;
+  std::exit(1);
 }
 
 int main(int argc, char *argv[]) {
-  long C_tripcount = atoi(argv[1]);
-  bool enable_profiling = (std::string{argv[2]} == "enable_profiling");
-  bool in_order = (std::string{argv[3]} == "in_order");
-  int n_queues = atoi(argv[4]);
-  std::vector<std::string> modes(argv + 5, argv + argc);
+  bool enable_profiling = false;
+  bool in_order = false;
+  bool out_of_order = false;
+  int n_queues = -1;
+  std::vector<std::string> commands;
+  long kernel_tripcount = 10000;
+
+  // I'm just an old C programmer trying to do some C++
+  for (int i = 1; i < argc; i++) {
+    std::string s{argv[i]};
+    if (s == "--enable_profiling") {
+      enable_profiling = true;
+    } else if (s == "--in_order") {
+      if (out_of_order)
+        print_help_and_exit("Cannot have --in_order and --out_of_order");
+      in_order = true;
+    } else if (s == "--out_of_order") {
+      if (in_order)
+        print_help_and_exit("Cannot have --in_order and --out_of_order");
+      out_of_order = true;
+    } else if (s == "--queues") {
+      i++;
+      if (i < argc) {
+        n_queues = std::stoi(argv[i]);
+      } else {
+        print_help_and_exit("Need to specify an value for --queues");
+      }
+    } else if (s == "--kernel_tripcount") {
+      i++;
+      if (i < argc) {
+        kernel_tripcount = std::stol(argv[i]);
+      } else {
+        print_help_and_exit("Need to specify an value for --kernel_tripcount");
+      }
+    } else if (s.rfind("-", 0) == 0) {
+      print_help_and_exit("Unsuported option: " + s);
+
+    } else {
+      static std::vector<std::string> command_supported = {"C", "MD", "DM"};
+      if (std::find(command_supported.begin(), command_supported.end(), s) ==
+          command_supported.end())
+        print_help_and_exit("Unsuported value for COMMAND");
+      commands.push_back(s);
+    }
+  }
+  if (n_queues == -1) {
+    if (in_order)
+      n_queues = 1;
+    else if (out_of_order)
+      n_queues = commands.size();
+  }
+  if (not(out_of_order || in_order) || commands.empty()) {
+    print_help_and_exit("Need to specify somme COMMAND and the order "
+                        "(--out_of_order or --in_order)");
+  }
 
   long serial_total_cpu_time;
   int serial_max_cpu_time_index_command;
   long serial_max_cpu_time_command;
-  bench<float>(modes, C_tripcount, enable_profiling, in_order, n_queues, true, &serial_total_cpu_time, &serial_max_cpu_time_command, &serial_max_cpu_time_index_command);
-  std::cout << "Total serial (us): " << serial_total_cpu_time 
-            << " (max commands (us) was " << modes[serial_max_cpu_time_index_command] << ": " << serial_max_cpu_time_command << ")" << std::endl;
-  const double expected  = 1. * serial_total_cpu_time / serial_max_cpu_time_command;
-  std::cout << "Expecting (assuming maximun concurency and negligeable runtime overhead) " << expected << "x speedup" << std::endl;
+  bench<float>(commands, kernel_tripcount, enable_profiling, in_order, n_queues,
+               true, &serial_total_cpu_time, &serial_max_cpu_time_command,
+               &serial_max_cpu_time_index_command);
+  std::cout << "Total serial (us): " << serial_total_cpu_time
+            << " (max commands (us) was "
+            << commands[serial_max_cpu_time_index_command] << ": "
+            << serial_max_cpu_time_command << ")" << std::endl;
+  const double expected =
+      1. * serial_total_cpu_time / serial_max_cpu_time_command;
+  std::cout << "Expecting (assuming maximun concurency and negligeable runtime "
+               "overhead) "
+            << expected << "x speedup" << std::endl;
   if (expected <= 1.30)
-    std::cerr<< " Warining: Large unblance between the commands... You may not want to trust bellow conclusion." << std::endl;
+    std::cerr << " Warining: Large unblance between the commands... You may "
+                 "not want to trust bellow conclusion."
+              << std::endl;
 
   long concurent_total_cpu_time;
-  bench<float>(modes, C_tripcount, enable_profiling, in_order, n_queues, false, &concurent_total_cpu_time, NULL, NULL);
+  bench<float>(commands, kernel_tripcount, enable_profiling, in_order, n_queues,
+               false, &concurent_total_cpu_time, NULL, NULL);
   std::cout << "Total // (us):     " << concurent_total_cpu_time << std::endl;
-  std::cout << "Got " << 1. * serial_total_cpu_time / concurent_total_cpu_time << "x speed-up relative to serial" << std::endl;
+  std::cout << "Got " << 1. * serial_total_cpu_time / concurent_total_cpu_time
+            << "x speed-up relative to serial" << std::endl;
   if (concurent_total_cpu_time <= 1.30 * serial_max_cpu_time_command) {
     std::cout << "SUCCESS: Concurent is faster than serial" << std::endl;
     return 0;
