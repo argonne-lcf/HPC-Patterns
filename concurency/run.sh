aade0248195ebe353b65787767cbad3dc6d07195
Thomas Applencourt <tapplencourt@anl.gov>
2022-03-28 17:21:44 +0000

    Update sycl_con

--- concurency/run.sh
+++ concurency/run.sh
@@ -2,15 +2,14 @@ set -o xtrace
 
 dpcpp sycl_con.cpp
 
-kernelTripcount=100000
 
 ( 
 for commands in "C C" "C MD" "C DM" "MD DM"
 do
   for mode in "out_of_order" "in_order"
   do
-    ./a.out $mode $commands --kernel_tripcount $kernelTripcount
-    ./a.out $mode $commands --kernel_tripcount $kernelTripcount --enable_profiling
+    ./a.out $mode $commands
+    ./a.out $mode $commands --enable_profiling
   done
 done
 ) |& tee run.log
--- concurency/sycl_con.cpp
+++ concurency/sycl_con.cpp
@@ -16,8 +16,12 @@
 
 #include <chrono>
 #include <iostream>
+#include <utility>
+#include <vector>
+#include <iomanip>
+
 #include <sycl/sycl.hpp>
-#define NUM_REPETION 8
+#define NUM_REPETION 10
 
 template <class T> T busy_wait(long N, T i) {
   T x = 1.3f;
@@ -29,40 +33,39 @@ template <class T> T busy_wait(long N, T i) {
 }
 
 template <class T>
-void bench(std::vector<std::string> commands, long kernel_tripcount,
-           bool enable_profiling, bool in_order, int n_queues, bool serial,
-           long *total_cpu_time, long *max_cpu_time_command,
-           int *max_index_cpu_time_command) {
+std::pair<long, std::vector<long>> bench(std::string mode, std::vector<std::string> commands, 
+          bool enable_profiling, int n_queues, long kernel_tripcount =0) {
 
+//   __
+//    |  ._  o _|_
+//   _|_ | | |  |_
+//
   const sycl::device D{sycl::gpu_selector()};
   const int globalWIs = D.get_info<sycl::info::device::sub_group_sizes>()[0];
-  const int N =
-      D.get_info<sycl::info::device::max_mem_alloc_size>() / sizeof(T);
-
+  const int N_copy = D.get_info<sycl::info::device::max_mem_alloc_size>() / sizeof(T);
   const sycl::context C(D);
+
+  // By default SYCL queue are out-of-order
   sycl::property_list pl;
-  if (enable_profiling && in_order)
+  if ( (mode == "in_order") && enable_profiling)
     pl = sycl::property_list{sycl::property::queue::in_order{},
                              sycl::property::queue::enable_profiling{}};
+  else if (mode == "in_order")
+    pl = sycl::property_list{sycl::property::queue::in_order{}};
   else if (enable_profiling)
     pl = sycl::property_list{sycl::property::queue::enable_profiling{}};
-  else if (in_order)
-    pl = sycl::property_list{sycl::property::queue::in_order{}};
 
-  std::vector<sycl::queue> Qs;
-  for (int i = 0; i < n_queues; i++)
-    Qs.push_back(sycl::queue(C, D, pl));
-
+  std::vector<sycl::queue> Qs(n_queues, sycl::queue(C, D, pl)) ;
+  //Initialize buffers according to the commands
   std::vector<std::vector<T *>> buffers;
   for (auto &command : commands) {
     std::vector<T *> buffer;
     for (const char &t : command) {
       T *b;
       if (t == 'M') {
-        b = static_cast<T *>(malloc(N * sizeof(T)));
-        std::fill(b, b + N, 0);
+        b = static_cast<T *>(calloc(N_copy,sizeof(T)));
       } else if (t == 'D') {
-        b = sycl::malloc_device<T>(N, D, C);
+        b = sycl::malloc_device<T>(N_copy, D, C);
       } else if (t == 'C') {
         b = sycl::malloc_device<T>(globalWIs, D, C);
       }
@@ -70,97 +73,101 @@ void bench(std::vector<std::string> commands, long kernel_tripcount,
     }
     buffers.push_back(buffer);
   }
+  long total_time = std::numeric_limits<long>::max();
+  std::vector<long> commands_times;
+  if (mode == "serial")
+    std::fill_n(std::back_inserter(commands_times), commands.size(), std::numeric_limits<long>::max());
 
-  *total_cpu_time = std::numeric_limits<long>::max();
+//    _
+//   |_)  _  ._   _ |_
+//   |_) (/_ | | (_ | |
+//
   for (int r = 0; r < NUM_REPETION; r++) {
-    std::vector<long> cpu_times;
     auto s0 = std::chrono::high_resolution_clock::now();
+    //Run all commands
     for (int i = 0; i < commands.size(); i++) {
       const auto s = std::chrono::high_resolution_clock::now();
       sycl::queue Q = Qs[i % n_queues];
-      sycl::event event;
       if (commands[i] == "C") {
         T *ptr = buffers[i][0];
         Q.parallel_for(globalWIs, [ptr, kernel_tripcount](sycl::item<1> j) {
           ptr[j] = busy_wait(kernel_tripcount, (T)j);
         });
       } else {
-        Q.copy(buffers[i][1], buffers[i][0], N);
+        Q.copy(buffers[i][1], buffers[i][0], N_copy);
       }
 
-      if (serial) {
+      if (mode == "serial") {
         Q.wait();
         const auto e = std::chrono::high_resolution_clock::now();
-        cpu_times.push_back(
-            std::chrono::duration_cast<std::chrono::microseconds>(e - s)
-                .count());
+        const auto curent_kernel_time = std::chrono::duration_cast<std::chrono::microseconds>(e - s).count();
+        commands_times[i] = std::min(commands_times[i], curent_kernel_time);
       }
     }
+    //Sync all queues
     for (auto &Q : Qs)
       Q.wait();
+    // Save time
     const auto e0 = std::chrono::high_resolution_clock::now();
-    const auto curent_total_cpu_time =
-        std::chrono::duration_cast<std::chrono::microseconds>(e0 - s0).count();
-    if (curent_total_cpu_time < *total_cpu_time) {
-      *total_cpu_time = curent_total_cpu_time;
-      if (serial) {
-        *max_index_cpu_time_command =
-            std::distance(cpu_times.begin(),
-                          std::max_element(cpu_times.begin(), cpu_times.end()));
-        *max_cpu_time_command = cpu_times[*max_index_cpu_time_command];
-      }
-    }
+    const auto curent_total_time = std::chrono::duration_cast<std::chrono::microseconds>(e0 - s0).count();
+    total_time = std::min(total_time, curent_total_time);
   }
+
+//    _
+//   /  |  _   _. ._      ._ 
+//   \_ | (/_ (_| | | |_| |_)
+//                        |
   for (const auto &buffer : buffers)
     for (const auto &ptr : buffer)
-      (sycl::get_pointer_type(ptr, C) != sycl::usm::alloc::unknown)
-          ? sycl::free(ptr, C)
-          : free(ptr);
+      //Shorter than to remember commands types...
+      (sycl::get_pointer_type(ptr, C) != sycl::usm::alloc::unknown) ? sycl::free(ptr, C) : free(ptr);
+
+  return std::make_pair(total_time, commands_times);
 }
 
 void print_help_and_exit(std::string binname, std::string msg) {
   if (!msg.empty())
     std::cout << "ERROR: " << msg << std::endl;
   std::string help = "Usage: " + binname +
-                     " (in_order | out_of_order)\n"
+                     " (in_order | out_of_order | serial)\n"
                      "                [--enable_profiling]\n"
                      "                [--n_queues=<queues>]\n"
                      "                [--kernel_tripcount=<tripcount>]\n"
                      "                COMMAND...\n"
                      "\n"
                      "Options:\n"
-                     "--kernel_tripcount       [default: 10000]\n"
-                     "--n_queues=<nqueues>     [default: -1]. Number of queues "
-                     "used to run COMMANDS. \n"
-                     "                                        If -1: one queue "
-                     "when out_of_order, one per COMMANDS when in order\n"
+                     "--kernel_tripcount       [default: -1]. Number of FMA per compute kernel\n"
+                     "                             '-1' mean autotuning of this parameter\n" 
+                     "--n_queues=<nqueues>     [default: -1]. Number of queues used to run COMMANDS\n"
+                     "                            '-1' mean automatic selection:\n"
+                     "                              - if `in_order`, one queue per COMMAND\n"
+                     "                              - else one queue\n"
                      "COMMAND                  [possible values: C,MD,DM]\n"
-                     "                            C:  Compute kernel \n"
-                     "                            MD: Malloc allocated memory "
-                     "to Device memory memcopy \n"
-                     "                            DM: Device Memory to Malloc "
-                     "allocated memory memcopy \n";
+                     "                            C:  Compute kernel\n"
+                     "                            MD: Malloc allocated memory to Device memory memcopy\n"
+                     "                            DM: Device Memory to Malloc allocated memory memcopy\n";
   std::cout << help << std::endl;
   std::exit(1);
 }
 
 int main(int argc, char *argv[]) {
+//    _                       _                                         
+//   |_) _. ._ _ o ._   _    /  |     /\  ._ _      ._ _   _  ._ _|_  _ 
+//   |  (_| | _> | | | (_|   \_ |_   /--\ | (_| |_| | | | (/_ | | |_ _> 
+//                      _|                   _|                         
+//
   std::vector<std::string> argl(argv + 1, argv + argc);
   if (argl.empty())
     print_help_and_exit(argv[0], "");
 
-  bool in_order = false;
-  if ((argl[0] != "out_of_order") && (argl[0] != "in_order"))
-    print_help_and_exit(argv[0], "Need to specify 'in_order' or 'out_of_order' option)");
-  else if (argl[0] == "in_order")
-    in_order = true;
-  // in_order = false; mean out_of_order. I know stupid optimization
+  std::string mode{argl[0]};
+  if ((mode != "out_of_order") && (mode != "in_order") && (mode != "serial"))
+    print_help_and_exit(argv[0], "Need to specify 'in_order', 'out_of_order', 'serial', option");
 
   bool enable_profiling = false;
   int n_queues = -1;
+  long kernel_tripcount = -1;
   std::vector<std::string> commands;
-  long kernel_tripcount = 10000;
-
   // I'm just an old C programmer trying to do some C++
   for (int i = 1; i < argl.size(); i++) {
     std::string s{argl[i]};
@@ -182,56 +189,60 @@ int main(int argc, char *argv[]) {
                             "Need to specify an value for '--kernel_tripcount'");
       }
     } else if (s.rfind("-", 0) == 0) {
-      print_help_and_exit(argv[0], "Unsuported option: '" + s + "'");
+      print_help_and_exit(argv[0], "Unsupported option: '" + s + "'");
     } else {
       static std::vector<std::string> command_supported = {"C", "MD", "DM"};
-      if (std::find(command_supported.begin(), command_supported.end(), s) ==
-          command_supported.end())
-        print_help_and_exit(argv[0], "Unsuported value for COMMAND");
+      if (std::find(command_supported.begin(), command_supported.end(), s) == command_supported.end())
+        print_help_and_exit(argv[0], "Unsupported value for COMMAND");
       commands.push_back(s);
     }
   }
-  if (n_queues == -1) {
-    if (in_order)
-      n_queues = 1;
-    else
-      n_queues = commands.size();
-  }
+  if (n_queues == -1)
+    n_queues = (mode == "in_order") ? commands.size() : 1;
+
   if (commands.empty())
-    print_help_and_exit(argv[0], "Need to specify somme COMMAND and the order "
-                                 "('--out_of_order' or '--in_order')");
+    print_help_and_exit(argv[0], "Need to specify COMMANDS (C,MD,DM)");
 
-  long serial_total_cpu_time;
-  int serial_max_cpu_time_index_command;
-  long serial_max_cpu_time_command;
-  bench<float>(commands, kernel_tripcount, enable_profiling, in_order, n_queues,
-               true, &serial_total_cpu_time, &serial_max_cpu_time_command,
-               &serial_max_cpu_time_index_command);
-  std::cout << "Total serial (us): " << serial_total_cpu_time
-            << " (max commands (us) was "
-            << commands[serial_max_cpu_time_index_command] << ": "
-            << serial_max_cpu_time_command << ")" << std::endl;
-  const double max_speedup =
-      1. * serial_total_cpu_time / serial_max_cpu_time_command;
-  std::cout << "Maximun Theoritical Speedup (assuming maximun concurency and "
-               "negligeable runtime overhead) "
-            << max_speedup << "x" << std::endl;
-  if (max_speedup <= 1.30)
-    std::cerr << "  WARNING: Large unblance between commands. Please play with '--kernel_tripcount' "
-              << std::endl;
-
-  long concurent_total_cpu_time;
-  bench<float>(commands, kernel_tripcount, enable_profiling, in_order, n_queues,
-               false, &concurent_total_cpu_time, NULL, NULL);
-  std::cout << "Total // (us):     " << concurent_total_cpu_time << std::endl;
-  std::cout << "Got " << 1. * serial_total_cpu_time / concurent_total_cpu_time
-            << "x speed-up relative to serial" << std::endl;
-
-  if (concurent_total_cpu_time <= 1.30 * serial_max_cpu_time_command) {
-    std::cout << "SUCCESS: Concurent is faster than serial" << std::endl;
-    return 0;
+//                                     __                
+//    /\     _|_  _ _|_     ._   _    (_   _  ._ o  _. | 
+//   /--\ |_| |_ (_) |_ |_| | | (/_   __) (/_ |  | (_| | 
+//                                                       
+  if (kernel_tripcount == -1 && (std::count(commands.begin(), commands.end(), "C"))) {
+    // We want each command to take the same time. We have only one parameter (kernel_tripcount) 
+    // In first approximation for the compute kernel T(kernel_time) -> elapsed_time is linear
+    long kernel_tripcount0 = 20000;
+    const auto& [copy_time, _1] = bench<float>("serial", {"MD"}, enable_profiling, n_queues);
+    const auto& [compute_time0, _2] = bench<float>("serial", {"C"}, enable_profiling, n_queues, kernel_tripcount0);
+    kernel_tripcount = (1.*kernel_tripcount0/compute_time0)*copy_time;
+    std::cout << "Autotuned Kernel Tripcount " << kernel_tripcount << std::endl;
   }
 
-  std::cout << "FAILURE: No Concurent Execution" << std::endl;
-  return 1;
+//    _                             __                   _       _                     
+//   /   _  ._ _  ._     _|_  _    (_   _  ._ o  _. |   |_)  _ _|_ _  ._ _  ._   _  _  
+//   \_ (_) | | | |_) |_| |_ (/_   __) (/_ |  | (_| |   | \ (/_ | (/_ | (/_ | | (_ (/_ 
+//                |                                                                    
+ const auto& [serial_total_time, serial_commands_times] = bench<float>("serial", commands, enable_profiling, n_queues, kernel_tripcount);
+ std::cout << "Total Time Serial " << serial_total_time << "us" << std::endl;
+ for (size_t i=0; i < commands.size(); i++)
+    std::cout << "  " << std::setw(2) << commands[i] <<  " " << serial_commands_times[i] << "us" << std::endl;
+
+ const double max_speedup = (1.* serial_total_time) / *std::max_element(serial_commands_times.begin(), serial_commands_times.end());
+ std::cout << "Maximum Theoretical Speedup " << max_speedup << "x" << std::endl;
+
+  if (max_speedup <= 1.80)
+    std::cerr << "  WARNING: Large Unbalance Between Commands" << std::endl;
+
+  const auto& [concurent_total_time, _] = bench<float>(mode, commands, enable_profiling, n_queues, kernel_tripcount);
+  std::cout << "Total Time // (us) " << concurent_total_time << "us" << std::endl;
+  const double speedup =  (1. * serial_total_time) / concurent_total_time;
+  std::cout << "Speedup Relative to Serial " << speedup << "x" << std::endl;
+
+
+  if (max_speedup >= 1.3*speedup) {
+    std::cout << "FAILURE: Far from Theoretical Speedup" << std::endl;
+    return 1;
+  }
+    
+  std::cout << "SUCCESS: Close from Theoretical Speedup" << std::endl;
+  return 0;
 }
